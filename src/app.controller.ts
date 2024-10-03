7a2575914d0cf3e0cb2d0c09c54bb501a71dddb7
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-10-03 12:52:54 +0200

    kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Controller, Get, Param, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { quotes } from './quotes';
 import { randomInt } from 'crypto';
-import { map } from 'rxjs';
 
 interface AuthorQuotes {
   author: string;
@@ -81,12 +80,12 @@ export class AppController {
     return {
       message: "BAZ+",
       idezet: {
-        quote:"Nincs ilyen elem!",
-        author:"",
-        img:""
+        quote: "Nincs ilyen elem!",
+        author: "",
+        img: "https://cdn.pixabay.com/photo/2020/12/30/01/45/smile-5872116_1280.png"
       }
     };
-    
+
   }
 
   @Get('deleteQuote/:id')
@@ -94,7 +93,7 @@ export class AppController {
   deleteqoute(@Param('id') id: string) {
     for (let element of quotes.quotes) {
       if (element.id == parseInt(id)) {
-        
+
         quotes.quotes.splice(quotes.quotes.indexOf(element), 1)
         return {
           message: "BAZ+",
@@ -102,11 +101,81 @@ export class AppController {
         };
       }
     }
-    
+
     return {
       message: "BAZ+",
       valasz: "Nincs ilyen elem!!!"
     };
   }
 
+  @Get('kereses')
+  @Render('search')
+  findtext(@Query('search') text: string = "") {
+    let lista = []
+
+    quotes.quotes.forEach(e => {
+      if (e.quote.includes(text) || e.author.includes(text)) {
+        lista.push(e)
+      }
+    });
+    return {
+      message: "BAZ+",
+      idezet: lista
+    };
+
+  }
+
+  @Get('authorRandomForm')
+  @Render('authorRandomForm')
+  authorRandomForm() {
+    return {
+      message: "BAZ+",
+    };
+  }
+  @Get('authorRandom')
+  @Render('authorRandom')
+  authorRandom(@Query("author") author: string) {
+    let x = []
+    quotes.quotes.forEach(element => {
+      if (element.author == author) {
+        x.push(element)
+      }
+    });
+    if (x.length == 0) {
+      return {
+        message: "BAZ+",
+        idezet: {
+          quote: "Nincs ilyen elem!",
+          author: "",
+          img: "https://cdn.pixabay.com/photo/2020/12/30/01/45/smile-5872116_1280.png"
+        }
+      };
+    } else {
+      let idezet = x[randomInt(x.length)]
+      return {
+        message: "BAZ+",
+        idezet: idezet
+      };
+    }
+  }
+
+  @Get('highlight/:id')
+  @Render('highlight')
+  highlight(@Query("text") text: string,@Param('id') id:string) {
+    
+    
+      if(quotes.quotes[parseInt(id)-1].quote.includes(text)){
+        quotes.quotes[parseInt(id)-1].quote.replace(text,"<strong>"+text+"</strong>")
+      }
+    
+
+    return{
+      message: "BAZ+",
+      idezet:quotes.quotes[parseInt(id)-1]
+    }
+
+  }
+
 }
+
+
